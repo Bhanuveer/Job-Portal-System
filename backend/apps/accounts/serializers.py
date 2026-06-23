5d9815e6f21db4b6c8e6683fabc3ddbd369e7924
Bhanuveer <bhanuveersingh2000@gmail.com>
2026-06-24 01:31:11 +0530

    feat: implement JWT authentication and profile API

--- backend/apps/accounts/serializers.py
+++ backend/apps/accounts/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import User
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class RegisterSerializer(serializers.ModelSerializer):
@@ -33,3 +35,50 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.save()
 
         return user
+    
+class LoginSerializer(serializers.Serializer):
+
+    email = serializers.EmailField()
+
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        user = authenticate(
+            username=email,
+            password=password
+        )
+
+        if not user:
+            raise serializers.ValidationError(
+                "Invalid email or password."
+            )
+
+        refresh = RefreshToken.for_user(user)
+
+        return {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+            "user": {
+                "id": user.id,
+                "name": user.name,
+                "email": user.email,
+                "role": user.role,
+            }
+        }
+    
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "name",
+            "email",
+            "role",
+            "phone_number",
+            "profile_image",
+        ]
--- backend/apps/accounts/views.py
+++ backend/apps/accounts/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .serializers import(RegisterSerializer, LoginSerializer,UserSerializer)
+from rest_framework.permissions import IsAuthenticated
 
 from .serializers import RegisterSerializer
 
@@ -20,7 +22,31 @@ class RegisterView(APIView):
                 status=status.HTTP_201_CREATED
             )
 
+    
+class LoginView(APIView):
+
+    def post(self, request):
+
+        serializer = LoginSerializer(data=request.data)
+
+        if serializer.is_valid():
+
+            return Response(
+                serializer.validated_data,
+                status=status.HTTP_200_OK
+            )
+
         return Response(
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
         )
+    
+class ProfileView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+
+        serializer = UserSerializer(request.user)
+
+        return Response(serializer.data)
